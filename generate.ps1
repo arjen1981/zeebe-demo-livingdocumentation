7838bb3fb9e7a5d437fd52c75984b205e691134e
Arjen van Benten <a.vanbenten@tfe.nl>
2021-11-04 08:30:51 +0100

    Changed namespace Adapters.Generated.Rest -> Adapters.Rest.Generated

--- generate.ps1
+++ generate.ps1
@@ -1,4 +1,4 @@
 docker run --rm -v "${PWD}:/local" openapitools/openapi-generator-cli generate `
     -i /local/docs/openapi.yaml `
     -g aspnetcore `
-    -o /local/dist/Adapters/Generated --additional-properties="aspnetCoreVersion=5.0,buildTarget=library,operationIsAsync=true,operationResultTask=true,packageName=Adapters.Generated.Rest,swashbuckleVersion=5.0.0"
+    -o /local/dist/Adapters/Generated --additional-properties="aspnetCoreVersion=5.0,buildTarget=library,operationIsAsync=true,operationResultTask=true,packageName=Adapters.Rest.Generated,swashbuckleVersion=5.0.0"
